0c669078f920ddecc80e1df567693c99c59a4d03
alan <asilvar4@correo.uss.cl>
2023-11-23 20:54:34 -0300

    example of filter function

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from datetime import datetime
+import pandas as pd
+
+df = pd.read_csv("database\\techpowerup_gpus.csv")
+
 
 # This is an example for the check function using the datatime library
-now = datetime.now()
-time = now.strftime("%d/%m/%Y %H:%M:%S")
-
 def check(action: str):
+    now = datetime.now()
+    time = now.strftime("%d/%m/%Y %H:%M:%S")
+    
     with open("logs.txt", "a") as f:
         f.write(f"{time}: {action}")
         
+# This is an example for the filter function
+def filter():
+    gpu_name = df.loc[df['graphics_processor.gpu_name'] == "Auburn"]
+    print(gpu_name)
+
 def restock():
     # ...
+    # At the end of the function, it will call to the check function to save the executed action in the logs.txt file
     check("Product 'id of the product''name of the product' has been restocked \n")
 
-restock()
