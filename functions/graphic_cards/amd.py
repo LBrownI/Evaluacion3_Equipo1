27d5f711e5f85c5a0785d57f0945c78116558478
Lucas Brown <129406202+LBrownI@users.noreply.github.com>
2023-12-04 02:24:47 -0300

    add Amd class

---
+++ functions/graphic_cards/amd.py
@@ -0,0 +1,87 @@
+import pandas as pd
+
+df = pd.read_csv("database\\techpowerup_gpus.csv")
+df.set_index("id", inplace=True)
+
+class Amd:
+    def __init__(self, df):
+        self.df = df
+        
+        self.df_with_filters = ""
+        self.selected_series = ""
+        
+        # This doesn't include all of the generations of each series (and not all graphics cards series either)
+        self.series_and_gen = {
+            "All-In-One": ["All-In-One(HD 6000)","All-In-One(HD 7000)","All-In-One(HD 8000)","All-In-One(Rx 200)","All-In-One(Rx 300)" ],
+            "Arctic Islands": ["Arctic Islands(R5 400)","Arctic Islands(R7 400)","Arctic Islands(RX 400)"],
+            "Bristol Ridge(Rx 300)": [],
+            "Carrizo(Rx 300 Mobile)": [],
+            "Cezanne": ["Cezanne(Vega Mobile)","Cezanne(Vega)"],
+            "Console GPU": ["Atari","Chuwi","Microsoft","Nintendo","Sony","Valve","Zhongshan Subor"],
+            "Embedded": [],
+            "FirePro": [],
+            "FireStream": [],
+            "Great Horned Owl(Vega)": [],
+            "Kabini": [],
+            "Kaveri": [],
+            "Lucienne": [],
+            "Mining GPUs": [],
+            "Mobility Radeon": [],
+            "Mullins": [],
+            "Navi II": [],
+            "Navi III": [],
+            "Navi(RX 5000)": [],
+            "Northern Islands": [],
+            "Palm": [],
+            "Picasso": [],
+            "Pirate Islands": [],
+            "Polaris": [],
+            "Radeon": ["Radeon Instinct","Radeon Pro Mac","Radeon Pro Mobile","Radeon Pro","Radeon Sky"],
+            "Raven Ridge": [],
+            "Rembrandt": [],
+            "Renoir": [],
+            "Richland": [],
+            "Sea Islands": [],
+            "Solar System": [],
+            "Southern Islands": [],
+            "Stoney Ridge": [],
+            "Sumo": [],
+            "Temash": [],
+            "Trinity": [],
+            "Vancouver": [],
+            "Vega": [],
+            "Volcanic Islands": [],
+            "Wrestler": [],
+
+
+
+            }
+
+    def series(self):
+        amd_series = list(self.series_and_gen.keys())
+        print("Select the series:")
+        for i, series in enumerate(amd_series):
+            print(f"  [{i+1}] {series}")
+
+        selected_series = int(input("\nInput: "))-1
+        self.selected_series = amd_series[selected_series]
+
+    def gen(self):
+        amd_gen = self.series_and_gen.get(self.selected_series)
+        
+        if amd_gen == []:
+            self.df_with_filters = self.df[self.df.graphics_card_generation.str.contains(self.selected_series)]
+            print(self.df_with_filters)
+        else:
+            print("\nSelect the generation:")
+            for i, gen in enumerate(amd_gen):
+                print(f"  [{i+1}] {gen}")
+            selected_gen = int(input("\nInput: "))-1
+
+            self.df_with_filters = self.df[self.df.graphics_card_generation.str.contains(self.series_and_gen.get(self.selected_series)[selected_gen])]
+            print(self.df_with_filters)
+a = Amd(df)
+a.series()
+a.gen()        
+
+   
