854491b9974193d3863eed0d333ed4727e221e3f
Lucas Brown <129406202+LBrownI@users.noreply.github.com>
2023-12-03 21:03:57 -0300

    update nvidia class

--- functions/graphic_cards/nvidia.py
+++ functions/graphic_cards/nvidia.py
@@ -12,9 +12,10 @@ class nvidia:
         
         # This doesn't include all of the generations of each series (and not all graphics cards series either)
         self.series_and_gen = {
-            "RTX": ["20", "30", "40"],
-            "Quadro": ["K", "M", "P"],
-            "GTX": ["9", "10"]
+            "Geforce": ["GeForce 1","GeForce 2","GeForce 3","GeForce 4","GeForce 5","GeForce 6"],
+            "Quadro": [],
+            "GTX": [],
+            "Riva": ["Riva"]
             }
 
     
@@ -22,7 +23,7 @@ class nvidia:
         nvidia_series = list(self.series_and_gen.keys())
         print("Select the series:")
         for i, series in enumerate(nvidia_series):
-            print(f"  [{i+1}]. {series}")
+            print(f"  [{i+1}] {series}")
 
         selected_series = int(input("\nInput: "))-1
         
@@ -37,11 +38,11 @@ class nvidia:
         nvidia_gen = self.series_and_gen.get(self.selected_series)
         print("Select the generation:")
         for i, gen in enumerate(nvidia_gen):
-            print(f"  [{i+1}]. {gen}")
+            print(f"  [{i+1}] {gen}")
 
         selected_gen = int(input("\nInput: "))-1
         
-        self.df_with_filters = self.df_with_filters[self.df_with_filters.gpu_name.str.contains(f"{self.selected_series} {nvidia_gen[selected_gen]}")]
+        self.df_with_filters = self.df_with_filters[self.df_with_filters.graphics_card.generation.str.contains(f"{self.selected_series} {nvidia_gen[selected_gen]}")]
         print(self.df_with_filters)
         
 a = nvidia(df)
