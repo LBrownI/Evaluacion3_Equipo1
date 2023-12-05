0afa40335657702c8dc81b4207026860b584a0c9
alan <asilvar4@correo.uss.cl>
2023-12-05 12:02:44 -0300

    add update function

---
+++ functions/update.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from functions.check import save_current_action
+
+def update(df):
+    answer = input("Do you want to update the Data Base (Yes/No)?: ").capitalize()
+    print(answer)
+    OPTIONS = ("Yes", "No")
+    while answer not in OPTIONS:
+        answer= input("Please just input 'Yes' or 'No': ").capitalize()
+    
+    match answer:
+        case "Yes":
+            new_df= input("Please input the new CSV's folder: ")
+            
+            try:
+                new_df= pd.read_csv(new_df)
+                new_df.set_index("id", inplace=True)
+                
+                df= new_df
+                print("The Data Base was updated")
+                return df
+            except:
+                print("The folder doesn´t exist, please verify it, verify the Data Base´s name or the back-slashes")
+            
+        case "No":
+            return df
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-from functions import check, restock, distribute, filter, graphic
+from functions import check, restock, distribute, filter, graphic, update
 
 df = pd.read_csv("database\\techpowerup_gpus.csv")
 df.set_index("id", inplace=True)
 
-OPTIONS = ("Graphic","Distribute","Restock","Check", "Filter","Exit")
+OPTIONS = ("Graphic","Distribute","Restock","Check", "Filter","Exit", "Update")
 
 def menu():
     option = input(f"\n*Welcome to inventory System*\nPlease choose one option:\n{OPTIONS}\n").capitalize()
@@ -27,6 +27,8 @@ while True:
             check.print_logs()
         case "Filter":
             filter.asjdiojasdjoiasd()
+        case "Update":
+            df = update.update(df)
         case "Exit":
             print("Saving files... (not really xd)")
             exit(0)
