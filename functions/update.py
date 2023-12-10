32f98925c6f5458e4eb26ead39041582984d4fbf
Vinbu <vincent.14.damian@gmail.com>
2023-12-10 16:44:57 -0300

    Update: Was fixed a logistic problem at the moment to save the new DateFrame in the DataBase

--- functions/update.py
+++ functions/update.py
@@ -2,31 +2,20 @@ import pandas as pd
 from functions.check import save_current_action
 
 def update(df):
-    answer = input("Do you want to update the Data Base (Yes/No)?: ").capitalize()
+    new_df = input("Please input the new CSV's folder: ")
 
-    OPTIONS = ("Yes", "No")
-    while answer not in OPTIONS:
-        answer= input("Please just input 'Yes' or 'No': ").capitalize()
-    
-    match answer:
-        case "Yes":
-            new_df= input("Please input the new CSV's folder: ")
-            
-            try:
-                new_df= pd.read_csv(new_df)
-                new_df.set_index("id", inplace=True)
+    try:
+        new_df = pd.read_csv(new_df)
+        new_df.set_index("id", inplace=True)
 
-                new_df_copy= new_df.copy()
+        new_df_copy = new_df.copy()
 
-                column_names = df.columns.tolist()
+        column_names = df.columns.tolist()
 
-                new_df = pd.merge(df, new_df, how='outer', on=column_names)
-                new_df = new_df.combine_first(new_df_copy)
-                df= new_df
-                print("The Data Base was updated")
-                return df
-            except:
-                print("The folder doesn't exist, please verify it, verify the Data Base's name or the back-slashes")
-            
-        case "No":
-            return df
+        new_df = pd.merge(df, new_df, how='outer', on=column_names)
+        new_df = new_df.combine_first(new_df_copy)
+        df = new_df
+        df.to_csv("database\\techpowerup_gpus.csv", index=False)
+        print("The Data Base was updated")
+    except:
+        print("The folder doesn't exist, please verify it, verify the Data Base's name or the back-slashes")
