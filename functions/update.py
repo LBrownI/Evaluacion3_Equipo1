fa37898052d543fd9c14259b90ea2fbab615a3f4
Lucas Brown <129406202+LBrownI@users.noreply.github.com>
2023-12-11 01:29:40 -0300

    fix update

--- functions/update.py
+++ functions/update.py
@@ -2,22 +2,21 @@ import pandas as pd
 from functions.check import save_current_action
 
 def update(df):
-    new_df = input("Please input the new CSV's folder location (path): ")
-
+    new_df_location = input("Please input the new CSV's folder location (path): ")
+    df = pd.read_csv("database\\techpowerup_gpus.csv")
     try:
-        new_df = pd.read_csv(new_df)
+        new_df = pd.read_csv(new_df_location)
+        if df is None or 'id' not in df.columns or 'id' not in new_df.columns:
+            raise ValueError("Both DataFrames must have the 'id' column.")
+        df.set_index("id", inplace=True)
         new_df.set_index("id", inplace=True)
-
-        new_df_copy = new_df.copy()
-
-        column_names = df.columns.tolist()
-
-        new_df = pd.merge(df, new_df, how='outer', on=column_names)
-        new_df = new_df.combine_first(new_df_copy)
-        df = new_df
-        df.to_csv("database\\techpowerup_gpus.csv", index=False)
-        print("The Data Base has been updated")
-    except:
-        print("The folder doesn't exist, please verify it, verify the Data Base's name or the back-slashes")
+        df.update(new_df)
+        df = pd.concat([df, new_df[~new_df.index.isin(df.index)]])
+        df.reset_index(inplace=True)
+        output_csv = input("Enter the output CSV file path (including file name and extension): ")
+        df.to_csv(output_csv, index=False)
+        print("The Database has been updated")
+    except Exception as e:
+        print(f"Error: {e}")
 
     save_current_action("[UPDATE] The working database has been updated")
