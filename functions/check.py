023984b39ed0e74c516c6593e599950f1c7a025f
Lucas Brown <129406202+LBrownI@users.noreply.github.com>
2023-12-11 02:04:49 -0300

    change check max display

--- functions/check.py
+++ functions/check.py
@@ -30,7 +30,7 @@ def print_logs():
             lines = file.readlines()
 
         start_line = len(lines) - 1
-        num_lines_displayed = 7
+        num_lines_displayed = 15
 
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
