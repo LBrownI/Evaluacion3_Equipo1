31d4942e2e8f0aec0cd7004efc3ec42f862b9579
alan <asilvar4@correo.uss.cl>
2023-12-11 01:34:23 -0300

    new desing for printing logs

--- functions/check.py
+++ functions/check.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import getpass
+import getpass, os
 
 def save_current_action(action: str):
     now = datetime.now()
@@ -10,5 +10,41 @@ def save_current_action(action: str):
         f.write(f"{time} - User: {username} - Action: {action}\n")
         
 def print_logs():
-     with open("logs\\logs.txt", "r") as f:
-        print(f.read())
+    if os.name == 'nt':
+        import msvcrt
+
+    def display_text(lines, start_line, num_lines):
+        for i in range(start_line, min(start_line + num_lines, len(lines))):
+            print(lines[i].rstrip())
+
+    def get_user_input():
+        if os.name == 'nt':
+            key = msvcrt.getch()
+            return key
+        else:
+            user_input = input("Press 'q' to exit the mode. + to move up. - to move down: ")
+            return user_input
+
+    def show_logs():
+        with open('logs\\logs.txt', 'r') as file:
+            lines = file.readlines()
+
+        start_line = len(lines) - 1
+        num_lines_displayed = 7
+
+        while True:
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+            display_text(lines, start_line, num_lines_displayed)
+
+            print("Press 'q' to exit the mode. '+' to move up. '-' to move down")
+            key = get_user_input()
+
+            if key == b'+' and start_line > 0:
+                start_line -= 1
+            elif key == b'-' and start_line < len(lines) - num_lines_displayed:
+                start_line += 1
+            elif key.lower() == b'q':
+                break
+
+    show_logs()
